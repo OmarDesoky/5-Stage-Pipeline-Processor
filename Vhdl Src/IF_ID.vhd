ab13ff559002f8da4c6f4135f7d13678a765233d
Omar <IamAdeadfish@hotmail.com>
2020-05-04 23:12:09 +0200

    [added] enable and instr_out in IF_ID

--- Vhdl Src/IF_ID.vhd
+++ Vhdl Src/IF_ID.vhd
@@ -7,12 +7,15 @@ entity if_id is
 port (
     clk : in std_logic;
     rst_async : in std_logic;
+    enable: in std_logic;
     last_taken_in : in std_logic;
     next_stall_in : in std_logic;
+    instruction_in: in std_logic_vector(16 downto 0);
     pc_in: in std_logic_vector(31 downto 0);
 
     last_taken_out : out std_logic;
     next_stall_out : out std_logic;
+    instruction_out: out std_logic_vector(16 downto 0);
     pc_out: out std_logic_vector(31 downto 0)
 );
 end if_id ;
@@ -22,17 +25,19 @@ architecture if_id_buffer of if_id is
 
 begin
 
-process(clk,rst_async)
+process(clk,rst_async,enable)
 begin
     if rst_async = '1' then
         last_taken_out <= '0';
         next_stall_out <= '0';
+        instruction_out <= (others => '0');
         pc_out <= (others => '0');
     
     else
-        if rising_edge(clk) then
+        if ( rising_edge(clk) and enable ='1' ) then
             last_taken_out <= last_taken_in;
             next_stall_out <= next_stall_in;
+            instruction_out <= instruction_in;
             pc_out <= pc_in;
         end if;
     end if;
