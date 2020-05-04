b0c438a7231d726bb2270544b5c1e9a541f77d9d
Omar <IamAdeadfish@hotmail.com>
2020-05-04 23:29:03 +0200

    [added] buffer enable and ea_imm enable

--- Vhdl Src/ID_EX.vhd
+++ Vhdl Src/ID_EX.vhd
@@ -7,6 +7,8 @@ entity id_ex is
 port (
     clk: in std_logic;
     rst_async: in std_logic;
+    buffer_enable: in std_logic;
+    ea_imm_enable: in std_logic;
     wb_in: in std_logic_vector(3 downto 0);
     mem_in: in std_logic_vector(3 downto 0);
     alu_op_in: in std_logic_vector(3 downto 0);
@@ -39,7 +41,7 @@ architecture id_ex_buffer of id_ex is
 
 begin
 
-process(clk,rst_async)
+process(clk,rst_async,ea_imm_enable,buffer_enable)
 begin
     if rst_async = '1' then
         wb_out <= (others => '0');
@@ -56,17 +58,21 @@ begin
     
     else
         if rising_edge(clk) then
-            wb_out <= wb_in;
-            mem_out <= mem_in;
-            alu_op_out <= alu_op_in;
-            ex_out <= ex_in;
-            data_1_out <= data_1_in;
-            data_2_out <= data_2_in;
-            src_1_out <= src_1_in;
-            src_2_out <= src_2_in;
-            ea_imm_out <= ea_imm_in;
-            pc_out <= pc_in;
-            dst_out <= dst_in;
+            if(ea_imm_enable = '1') then
+                ea_imm_out <= ea_imm_in;
+            elsif buffer_enable = '1' then
+                wb_out <= wb_in;
+                mem_out <= mem_in;
+                alu_op_out <= alu_op_in;
+                ex_out <= ex_in;
+                data_1_out <= data_1_in;
+                data_2_out <= data_2_in;
+                src_1_out <= src_1_in;
+                src_2_out <= src_2_in;
+                pc_out <= pc_in;
+                dst_out <= dst_in;
+            end if;    
+
         end if;
     end if;
 
