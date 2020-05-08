c4a17a7f70e98d648641e704c65c72434dc270df
Ahmed Waleed Amin <ahmdwlydahmd09@gmail.com>
2020-05-08 22:29:52 +0200

    Update Fetch stage

--- Vhdl Src/Fetch Stage/Fetch_Stage.vhd
+++ Vhdl Src/Fetch Stage/Fetch_Stage.vhd
@@ -34,10 +34,10 @@ BEGIN
  
  PC_WB2 <= pc_wb and (not(INT_raised_before_test));
  pc_Select : entity work.pc_selector
-    port map(rst_async=>rst_async_test, int=>int_test, pc_wb=>PC_WB2, take_jmp_addr=> take_jmp_addr_test, mem_loc_2_3=>mem_loc_2_3_test
+    port map(rst_async=>rst_async_test, int=>INT_2nd_cycle_test, pc_wb=>PC_WB2, take_jmp_addr=> take_jmp_addr_test, mem_loc_2_3=>mem_loc_2_3_test
 , pc_frm_wb=>pc_frm_wb_test ,calc_jmp_addr=>calc_jmp_addr_test, pc_updated=>pc_updated_test, pc_out=>pc_selected);
 
- write_enb_test <= (write_enb_from_INT and PC_ENB_DATAHAZARD);
+ write_enb_test <= (write_enb_from_INT and PC_ENB_DATAHAZARD and (not int_test));
 
  pc_REG : entity work.n_bit_register
     port map(CLK, rst_async=>rst_async_test, write_enb=>write_enb_test, d=>pc_selected, q=>pc_out_memory_in);
@@ -50,7 +50,7 @@ BEGIN
     port map(CLK, '0', '1', pc_out_memory_in, "0000000000000000",dataout_Memory, mem_loc_2_3_test);
 
  PC_SAVE_DET : entity work.PC_Save_Determine
-    port map(pc_out_memory_in, pc_updated_test, IN_MIDDLE_OF_IMM, IF_ANY_JUMP, CLK,PC_OUT_SAVE_Determine);
+    port map(pc_out_memory_in, pc_updated_test, IN_MIDDLE_OF_IMM, IF_ANY_JUMP,PC_OUT_SAVE_Determine);
 
 
 END flow; 
