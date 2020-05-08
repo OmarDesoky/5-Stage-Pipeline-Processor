61bcb175367eac7cd30a23e2d22a21cb9f2185fb
Ahmed Waleed Amin <ahmdwlydahmd09@gmail.com>
2020-05-08 21:36:54 +0200

    Update Pc save determine , upload fetch stage

--- Vhdl Src/PC Save Determine/PC_Save_Determine.vhd
+++ Vhdl Src/PC Save Determine/PC_Save_Determine.vhd
@@ -11,27 +11,24 @@ END ENTITY PC_Save_Determine;
 
 ARCHITECTURE behavioral_flow OF PC_Save_Determine IS
 
-signal q1_SIG:std_logic_vector(31 downto 0);
+--signal q1_SIG:std_logic_vector(31 downto 0);
 
-signal q2_SIG:std_logic_vector(63 downto 0);
+--signal q2_SIG:std_logic_vector(63 downto 0);
 BEGIN   
 
-process(clk)
+process(clk, CURRENT_PC, UPDATED_PC,IN_MIDDLE_OF_IMM, IF_ANY_JUMP)
+variable q1_SIG:std_logic_vector(31 downto 0);
+variable q2_SIG:std_logic_vector(63 downto 0);
 begin 
--- writing the results in the rising edge 
-if rising_edge(clk) then  
-	if ((IN_MIDDLE_OF_IMM = '1') or (IF_ANY_JUMP = '1')) then
-		PC_OUT <= q2_SIG(63 downto 32);
-	else 
-		PC_OUT <= q1_SIG;
-	end if;
+if ((IN_MIDDLE_OF_IMM = '1') or (IF_ANY_JUMP = '1')) then
+	PC_OUT <= q2_SIG(63 downto 32);
+else 
+	PC_OUT <= UPDATED_PC;
 end if;
--- reading data in the falling edge
-if falling_edge(clk) then 
-	q2_SIG(63 downto 32) <= q2_SIG(31 downto 0);
-	q2_SIG(31 downto 0) <= CURRENT_PC;
-	q1_SIG <= UPDATED_PC;
-end if;
+q2_SIG(63 downto 32) := q2_SIG(31 downto 0);
+q2_SIG(31 downto 0) := CURRENT_PC;
+q1_SIG := UPDATED_PC;
 end process;
+
 
 END behavioral_flow;
