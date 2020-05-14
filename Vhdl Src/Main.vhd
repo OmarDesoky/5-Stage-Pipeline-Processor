39fe8580ce4491e17e8975450cc86d79b67d3357
sha3er97 <sha3er97@github.com>
2020-05-14 23:36:49 +0200

    add to main

--- Vhdl Src/Main.vhd
+++ Vhdl Src/Main.vhd
@@ -28,7 +28,7 @@ architecture Arch of processor is
     signal PC_FROM_prediction :std_logic_vector(31 downto 0);
 
     --from WB
-    signal pc_wb_FROM_WB :std_logic;
+    signal pc_wb_FROM_WB,swap_wb_FROM_WB,reg_wb_FROM_WB :std_logic;
     signal PC_FROM_WB :std_logic_vector(31 downto 0);
 
     --from fetch
@@ -36,18 +36,29 @@ architecture Arch of processor is
     signal PC_FROM_fetch :std_logic_vector(31 downto 0);
     signal INT_FROM_fetch :std_logic;
 
+    -- from data hazard "to be used later"
+    signal PC_ENB_FROM_DATAHAZARD : std_logic := '1' ;
+
 
 begin
     fetch : entity work.Fetch_Stage
-        port map(rst_async_test=>RST,int_test=>INT, pc_wb =>pc_wb_FROM_WB,take_jmp_addr_test=>take_jmp_addr_FROM_prediction
-        ,IN_MIDDLE_OF_IMM=>IN_MIDDLE_OF_IMM_FROM_decode,IF_ANY_JUMP=>IF_ANY_JUMP_FROM_decode,CLK,flip_next_cycle_INT_test=>IF_ID_ENB
-        ,PC_ENB_DATAHAZARD=>'1',Flush=>Flush_FROM_decode,pc_frm_wb_test=>PC_FROM_WB,calc_jmp_addr_test=>PC_FROM_prediction,
-        instruction=>instruction_fetched,PC_Saved=>PC_FROM_fetch,INT_First_Cycle=>INT_FROM_fetch);
+        port map(rst_async_test=>RST,int_test=>INT, pc_wb =>pc_wb_FROM_WB
+        ,take_jmp_addr_test=>take_jmp_addr_FROM_prediction
+        ,IN_MIDDLE_OF_IMM=>IN_MIDDLE_OF_IMM_FROM_decode,IF_ANY_JUMP=>IF_ANY_JUMP_FROM_decode
+        ,CLK,flip_next_cycle_INT_test=>IF_ID_ENB
+        ,PC_ENB_DATAHAZARD=>PC_ENB_FROM_DATAHAZARD,Flush=>Flush_FROM_decode
+        ,pc_frm_wb_test=>PC_FROM_WB,calc_jmp_addr_test=>PC_FROM_prediction
+        ,instruction=>instruction_fetched,PC_Saved=>PC_FROM_fetch
+        ,INT_First_Cycle=>INT_FROM_fetch);
 
     IF_ID : entity work.if_id
-        port map(CLK,RST,enable=>IF_ID_ENB,last_taken_in=>last_taken_FROM_prediction,next_stall_in=>next_stall_FROM_prediction
-        ,int_in=>INT_FROM_fetch,instruction_in=>instruction_fetched,pc_in=>PC_FROM_fetch,last_taken_out=>last_taken_TO_decode
-        ,next_stall_out=>next_stall_TO_decode,int_out=>int_TO_decode,instruction_out=>instruction_TO_decode,pc_out=>pc_TO_decode);
+        port map(CLK,RST,enable=>IF_ID_ENB,last_taken_in=>last_taken_FROM_prediction
+        ,next_stall_in=>next_stall_FROM_prediction
+        ,int_in=>INT_FROM_fetch,instruction_in=>instruction_fetched
+        ,pc_in=>PC_FROM_fetch,last_taken_out=>last_taken_TO_decode
+        ,next_stall_out=>next_stall_TO_decode,int_out=>int_TO_decode
+        ,instruction_out=>instruction_TO_decode,pc_out=>pc_TO_decode);
 
-    
+    decode : entity work.decode_stage
+        port map(CLK,RST,
 end Arch ; -- Arch
