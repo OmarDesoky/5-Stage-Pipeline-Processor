44187ec444d1867e575b429a07e4b2f2c6356c6e
sha3er97 <sha3er97@github.com>
2020-05-18 16:46:05 +0200

    add FW test case

--- Assembler/input.asm
+++ Assembler/input.asm
@@ -9,23 +9,12 @@
 100
 
 .ORG 10
-#setC           #C --> 1
-NOP            #No change
-#ClRC           #C --> 0
-NOT R1         #R1 =FFFFFFFF , C--> no change, N --> 1, Z --> 0
 inc R1	       #R1 =00000000 , C --> 1 , N --> 0 , Z --> 1
-in R1	       #R1= 5,add 5 on the in port,flags no change	
-in R2          #R2= 10,add 10 on the in port, flags no change
-NOT R2	       #R2= FFFFFFEF, C--> no change, N -->1,Z-->0
-inc R1         #R1= 6, C --> 0, N -->0, Z-->0
-Dec R2         #R2= FFEE,C-->1 , N-->1, Z-->0
-add r1,r2,r3
-sub r2,r3, r7
+inc R1
+inc R2	       #R2= FFFFFFEF, C--> no change, N -->1,Z-->0
+inc R2         #R1= 6, C --> 0, N -->0, Z-->0
+inc R2         #R2= FFEE,C-->1 , N-->1, Z-->0
+add r1,r2,r4
+sub r4,r2,r5
+swap r4,r5
 not r5
-and r3,r4,r5
-out R1
-out R2
-ldm R2,100
-ldd r2,100
-std r1,50
-shr r5,100
