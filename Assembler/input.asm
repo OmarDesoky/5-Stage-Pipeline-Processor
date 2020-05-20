56bdb53a856681fc82758010bf46a11e39a18d1f
sha3er97 <sha3er97@github.com>
2020-05-20 20:41:21 +0200

    jump test cases

--- Assembler/input.asm
+++ Assembler/input.asm
@@ -19,35 +19,30 @@ in R7     #R7=FFFFFFFF
 Push R4   #sp=7FC, M[7FE, 7FF]=300
 JMP R1 
 INC R7	  # this statement shouldn't be executed,
- 
 #check flag forwarding  
 .ORG 30
 AND R1,R5,R5   #R5=0 , Z = 1
-            #try interrupt here
+#try interrupt here
 JZ  R2      #Jump taken, Z = 0
 INC R7      #this statement shouldn't be executed
-
 #check on flag updated on jump
 .ORG 50
 JZ R3      #Jump Not taken
-
 #check destination forwarding
 NOT R5     #R5=FFFFFFFF, Z= 0, C--> not change, N=1
 INC R5     #R5=0, Z=1, C=1, N=0
 in  R6     #R6=200, flag no change
 JZ  R6     #jump taken, Z = 0
 INC R1   #this statement shouldn't be executed
-
 .ORG 100
 ADD R0,R0,R0    #N=0,Z=1,C=0
 out R6
 rti
-
 #check on load use
 .ORG 200
 POP R6     #R6=300, SP=7FE
 Call R6    #SP=7FC, M[7FF]=half next PC,M[7FE]=other half next PC
-          #try interrupt here
+#try interrupt here
 INC R6	  #R6=401, this statement shouldn't be executed till call returns, C--> 0, N-->0,Z-->0
 NOP
 NOP
